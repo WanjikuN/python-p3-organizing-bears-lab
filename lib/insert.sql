4e2aa1ecbe551d316f082a8b13c6daa2fd9eca9a
WanjikuN <cikunjoroge4@gmail.com>
2023-09-23 11:40:54 +0300

    feat: add CRUD operations

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES('Mr. Chocolate',2,'M','Brown','xy',1);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES('MRowdy',4,'F','Brown','xy',1);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES('Tabitha',10,'M','Brown','xy',1);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES('Sergeant Brown',21,'M','Brown','x',0);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES('Melissa',12,'F','Black','xy',0);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES('Grinch',40,'M','Brown','y',1);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES('Wendy',9,'F','Red','xfy',0);
+
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES(NULL,2,'M','White','xwy',1);
